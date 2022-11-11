44977506bbf375b69017ac02147b279aa72089ce
frankwear <broaduse@down2thewire.net>
2022-11-10 21:15:12 -0500

    Fix random errors preventing app start

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.scss';
 import App from './components/App';
-import { configureStore } from '@reduxjs/toolkit'
-import {rootReducer} from './utils/Redux'
-import {Provider} from "react-redux";
+//import { configureStore } from '@reduxjs/toolkit'
+//import {rootReducer} from './utils/Redux'
+//import {Provider} from "react-redux";
 
-const store = configureStore({
-    reducer: rootReducer,
-})
+//const store = configureStore({
+//    reducer: rootReducer,
+//})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
